9ea96755417fecc83d4a3dc050f1ab06957004c1
kaiddd <dinghaokai@gmail.com>
2024-08-05 10:13:46 +0800

    v01

--- test.py
+++ test.py
@@ -6,13 +6,14 @@ voyager = Voyager(
     #azure_login=azure_login,
     mc_port=65535,
     env_wait_ticks=100,
-    env_request_timeout=800,
+    env_request_timeout=1500,
     # 获取环境变量的值
-    openai_api_key = os.getenv("OPENAI_API_KEY")
+    openai_api_key = os.getenv("OPENAI_API_KEY"),
+    openai_api_request_timeout=1000,
     #skill_library_dir="./skill_library/trial1", # Load a learned skill library.
     #ckpt_dir="./ckpt2", # Feel free to use a new dir. Do not use the same dir as skill library because new events will still be recorded to ckpt_dir. 
     #resume=False, # Do not resume from a skill library because this is not learning.
-    #resume=True,
+    resume=True,
 
 )
 # 运行任务分解
--- voyager/env/mineflayer/index.js
+++ voyager/env/mineflayer/index.js
@@ -27,7 +27,7 @@ app.post("/start", (req, res) => {
     bot = null;
     console.log(req.body);
     bot = mineflayer.createBot({
-        host: "localhost", // minecraft server ip
+        host: "192.168.50.226", // minecraft server ip
         port: req.body.port, // minecraft server port
         username: "bot",
         disableChatSigning: true,
