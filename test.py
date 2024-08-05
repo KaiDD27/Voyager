a19c3c4d86ddccfbb167ea467d4071ca84fade36
kaiddd <dinghaokai@gmail.com>
2024-08-05 23:23:32 +0800

    vv02

--- test.py
+++ test.py
@@ -6,10 +6,10 @@ voyager = Voyager(
     #azure_login=azure_login,
     mc_port=65535,
     env_wait_ticks=100,
-    env_request_timeout=1500,
+    env_request_timeout=1000,
     # 获取环境变量的值
     openai_api_key = os.getenv("OPENAI_API_KEY"),
-    openai_api_request_timeout=1000,
+    openai_api_request_timeout=500,
     #skill_library_dir="./skill_library/trial1", # Load a learned skill library.
     #ckpt_dir="./ckpt2", # Feel free to use a new dir. Do not use the same dir as skill library because new events will still be recorded to ckpt_dir. 
     #resume=False, # Do not resume from a skill library because this is not learning.
